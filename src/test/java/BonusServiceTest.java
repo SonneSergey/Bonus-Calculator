bfdb58bcbe931fda4ae8b37a73c2f3a54a8ac10d
Sergey_Sonne <berit2008@mail.ru>
2024-10-29 06:04:53 +0300

    Добавлены изменения в  src/test/java/BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,15 +7,15 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+        // Подготавливаем данные:
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
 
-        // вызываем целевой метод:
+        // Вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+        // Производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
@@ -23,51 +23,65 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+        // Подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
         long expected = 500;
 
-        // вызываем целевой метод:
+        // Вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+        // Производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
-    // Тесты для незарегистрированных пользователей и для случаев, когда сумма меньше 100
+    // Тесты для незарегистрированных пользователей,когда сумма меньше 100
     @Test
     void shouldCalculateForUnregisteredAndUnderLimit() {
         BonusService service = new BonusService();
-        // подготавливаем данные:
-        long amount = 1000;
+        // Подготавливаем данные:
+        long amount = 50;
         boolean registered = false;
-        long expected = 10;
+        long expected = 0;
 
-        // вызываем целевой метод:
+        // Вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+        // Производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
+    // Тест для незарегистрированных пользователей и суммы равной 100
+    @Test
+    void shouldCalculateForUnregisteredAndExactly100() {
+        BonusService service = new BonusService();
+        // Подготавливаем данные:
+        long amount = 100;
+        boolean registered = false;
+        long expected = 1;
+        // Вызывваем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // Производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Тест для незарегистрированных пользователей и суммы равной 50.000
     @Test
     void shouldCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
-        // подготавливаем данные:
+        // Подготавливаем данные:
         long amount = 50_000;
         boolean registered = false;
         long expected = 500;
 
-        //  вызываем целевой метод:
+        //  Вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+        // Производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
-    // Тест для случая, когда сумма покупки у незарегистрированного пользователя превышает лимит
-
+    // Тест для случаев, когда сумма покупки у незарегистрированного пользователя превышает лимит
     @Test
     void shouldCalculateForUnregisteredAndLargePurchase() {
         BonusService service = new BonusService();
